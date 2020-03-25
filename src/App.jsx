a4a952512fe58540de43add2d31460aa293d19b4
yuan xu <a2589654@gmail.com>
2020-03-25 13:33:29 -0700

    login and home router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Admin from "./pages/admin/admin";
+import Login from "./pages/login/login";
 
 export default class App extends Component{
-    
+
     render(){
         return(
-            <div>
-                app
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login} />
+                    <Route path='/' component={Admin} />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
