ef0f671dd330c1df10538cb20c802441d585b9c3
yuan xu <a2589654@gmail.com>
2020-03-28 00:46:31 -0700

    category page

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,10 +1,55 @@
 import React, {Component} from 'react';
+import { Card, Button, Table } from 'antd'
+import { PlusCircleOutlined } from '@ant-design/icons';
 
+import ButtonLink from "../../components/button-link/button-link";
 export default class Category extends Component{
 
     render(){
+        const title = 'Level I';
+        const extra = (
+            <Button type='primary'>
+                <PlusCircleOutlined/>
+                Create
+            </Button>
+        );
+        const dataSource = [
+            {
+                key: '1',
+                name: '胡彦斌',
+                age: 32,
+                address: '西湖区湖底公园1号',
+            },
+            {
+                key: '2',
+                name: '胡彦祖',
+                age: 42,
+                address: '西湖区湖底公园1号',
+            },
+        ];
+
+        const columns = [
+            {
+                title: '姓名',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: 'Edit',
+                width: 300,
+                render: () =>(
+                    <span>
+                        <ButtonLink>Rename</ButtonLink>
+                        <ButtonLink>Subcategory</ButtonLink>
+                    </span>
+                )
+            }
+        ];
+
         return(
-            <div>Category</div>
+            <Card title={title} extra={extra}>
+                <Table bordered dataSource={dataSource} columns={columns} />
+            </Card>
         )
     }
 }
